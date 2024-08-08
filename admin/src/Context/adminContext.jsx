8f850db62b3276c4d612a3b56764513e6c5305ce
Dev Harsh <devharsh213@gmail.com>
2024-08-08 09:55:55 +0530

    Backend URL Updated

--- admin/src/Context/adminContext.jsx
+++ admin/src/Context/adminContext.jsx
@@ -3,14 +3,15 @@ export const AdminContext = createContext();
 let port = 4000;
 
 const AdminContextProvider = (props) => {
-    let url = `http://localhost:${port}`
-
+    let url = `https://fashion-cave-backend.onrender.com`
+    //http://localhost:${port}
+    
     const contextValue = {url};
     return(
         <AdminContext.Provider value={contextValue}>
             {props.children}
         </AdminContext.Provider>
     );
-};
+}; 
 
 export default AdminContextProvider;
